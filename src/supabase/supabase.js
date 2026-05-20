dc68c1730049c8b6e06f598f3744dbfc749f180f
Yassine El Atlassi <55562792+Yapacdev@users.noreply.github.com>
2026-05-20 14:55:08 +0100

    Update Supabase client initialization and keys

--- src/supabase/supabase.js
+++ src/supabase/supabase.js
@@ -2,12 +2,9 @@ import { createClient } from '@supabase/supabase-js'
 
 
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+const supabaseKey = import.meta.env.VITE_SUPABASE_PUBLISHABLE_KEY;
 
-console.log('Supabase URL:', supabaseUrl)
-console.log('Supabase Anon Key:', supabaseAnonKey)
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey)
+export const supabase = createClient(supabaseUrl, supabaseKey)
 
 export const CATEGORIES = {
   toprock: { label: 'Toprock', color: '#E85D3A', icon: '🕴' },
